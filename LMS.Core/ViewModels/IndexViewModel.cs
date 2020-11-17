71cc176b36b6f1c42a0f0b022967e03e607243fc
Iván Araque Cristóbal <ivanaraquecristobal@gmail.com>
2020-11-17 16:50:45 +0100

    small changes part 2

--- LMS.Core/ViewModels/IndexViewModel.cs
+++ LMS.Core/ViewModels/IndexViewModel.cs
@@ -6,13 +6,13 @@ namespace LMS.Core.ViewModels
 {
     public class IndexViewModel
     {
-        public string Name { get; set; }
+        //public string Name { get; set; }
 
-        public string Description { get; set; }
+        //public string Description { get; set; }
 
-        public DateTime StartDate { get; set; }
+        //public DateTime StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
+        //public DateTime EndDate { get; set; }
         public IEnumerable<ModulesViewModel> Modules { get; set; }
     }
 }
