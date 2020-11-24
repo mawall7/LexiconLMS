e4352dc8bbd39edd239e989305037b03090518b0
Mattias Wallerman <wallermanmattias@yahoo.se>
2020-11-24 17:58:39 +0100

    Save | Cancel -  transparent Buttons

--- LMS.Core/Entities/Activity.cs
+++ LMS.Core/Entities/Activity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace LMS.Core.Entities
@@ -7,10 +8,17 @@ namespace LMS.Core.Entities
     public class Activity
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public DateTime StartTime { get; set; }
 
+        [Required]
         public DateTime EndTime { get; set; }
 
         // Foreign key
