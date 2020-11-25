e91641f8cd58dad8ae7368b93dda11ccbb2a5bc3
Fatma Alhouthi <fa222nx@student.lnu.se>
2020-11-25 11:43:05 +0100

    resolve konflicts

--- LMS.Web/Program.cs
+++ LMS.Web/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using LMS.Data.Data;
 using Microsoft.Extensions.DependencyInjection;
-
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
