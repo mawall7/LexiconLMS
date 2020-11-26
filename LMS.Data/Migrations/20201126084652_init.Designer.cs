ed7e9b5d2c20518c04b1dd790047ea67fcabe76f
Iván Araque Cristóbal <ivanaraquecristobal@gmail.com>
2020-11-26 09:49:03 +0100

    test presentation

--- LMS.Data/Migrations/20201126084652_init.Designer.cs
+++ LMS.Data/Migrations/20201126084652_init.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LMS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20201124130400_init")]
+    [Migration("20201126084652_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
